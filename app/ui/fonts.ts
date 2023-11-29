00374abafabdfe6f9c052603aa3e5c5f03df7a64
Luis Magalhaes <luis.magalhaes@theblueground.com>
2023-11-29 12:32:07 -0300

    Styling, custom fonts and Image component

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Space_Grotesk } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+export const space_grotesk = Space_Grotesk({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+});
